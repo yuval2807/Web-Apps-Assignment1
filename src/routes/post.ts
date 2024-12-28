06e8b55e9fbe07c4149b413e45aa84dc3989ffcd
inbar112 <inbaradany@gmail.com>
2024-12-28 18:04:45 +0200

    added swagger to auth and post

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -8,10 +8,71 @@ import {
 } from "../controllers/post";
 import authenticateToken from "../middleware/jwt";
 
+/**
+ * @swagger
+ * tags:
+ *  name: Posts
+ *  description: The posts API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *       Post:
+ *           type: object
+ *           properties:
+ *               _id:
+ *                   type: string
+ *               title:
+ *                   type: string
+ *               content:
+ *                   type: string
+ *               sender:
+ *                   type: string
+ *           example:
+ *              _id: 'hgsfjhskljslkgl2kgldjd'
+ *              title: 'example title'
+ *              content: 'example content'
+ *              owner: 'adraaggayajala'
+ *       PostBody:
+ *           type: object
+ *           required:
+ *              - title
+ *           properties:
+ *               title:
+ *                   type: string
+ *               content:
+ *                   type: string
+ *           example:
+ *              title: 'example title'
+ *              content: 'example content'
+ */
+
 const router = express.Router();
 
 router.use(authenticateToken);
 
+/**
+ * @swagger
+ * /post:
+ *   get:
+ *       summary: Retrieve a list of all posts
+ *       tags: [Posts]
+ *       security:
+ *           - bearerAuth: []
+ *       responses:
+ *           200:
+ *               description: A list of posts
+ *               content:
+ *                   application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Post'
+ *           400:
+ *              description: Bad request
+ */
 router.get("/", async (req: Request, res: Response) => {
   const sender = req.query.sender;
 
@@ -25,6 +86,32 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @swagger
+ * /post/{post_id}:
+ *   get:
+ *       summary: Retrieve a post by id
+ *       tags: [Posts]
+ *       security:
+ *           - bearerAuth: []
+ *       parameters:
+ *          - name: post_id
+ *            in: path
+ *            required: true
+ *            schema:
+ *              type: string
+ *       responses:
+ *           200:
+ *               description: A specific post
+ *               content:
+ *                   application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Post'
+ *           400:
+ *              description: Bad request
+ *           404:
+ *              description: Not Found
+ */
 router.get("/:post_id", async (req: Request, res: Response) => {
   const id = req.params.post_id;
 
